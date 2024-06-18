602e66d637d35092d53cd1884c19b19d7afd64e3
John Barrett <john@jbrt.org>
2024-06-18 18:33:14 +0100

    Zeroth iteration of SunVox alien installer

---
+++ lib/Alien/SunVox.pm
@@ -0,0 +1,15 @@
+use strict;
+use warnings;
+package Alien::SunVox;
+
+# ABSTRACT: Install SunVox
+
+our $VERSION = '0.00';
+
+use parent qw/ Alien::Base /;
+
+# ALIEN SYNOPSIS
+# ALIEN DESCRIPTION
+# ALIEN SEE ALSO
+
+1;
