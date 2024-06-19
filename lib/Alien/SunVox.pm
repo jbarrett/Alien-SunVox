1639b9606b1334b3b60c1747b2f126c8b654f090
John Barrett <john@jbrt.org>
2024-06-19 21:54:05 +0100

    ack NightRadio

--- lib/Alien/SunVox.pm
+++ lib/Alien/SunVox.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Alien::SunVox;
 
-# ABSTRACT: Install The SunVox Library
+# ABSTRACT: Install The SunVox Library - Alexander Zolotov's SunVox modular synthesizer and sequencer
 
 our $VERSION = '0.00';
 
@@ -11,5 +11,12 @@ use parent qw/ Alien::Base /;
 # ALIEN SYNOPSIS
 # ALIEN DESCRIPTION
 # ALIEN SEE ALSO
+
+=head1 ACKNOWLEDGEMENT
+
+Powered by SunVox (modular synth & tracker)
+Copyright (c) 2008 - 2024, Alexander Zolotov <nightradio@gmail.com>, WarmPlace.ru
+
+=cut
 
 1;
